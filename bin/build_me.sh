c39dd2af38c574e94b3e5f2ba652bbe0e1bb0694
PipGrylls <pipgryllsastro@gmail.com>
2022-08-04 11:46:04 +0000

    Deploying to gh-pages from @ SRSG-Workshops/Doctoral-College-8-22@59c0eb254ebfab1918b2ef64924127ed5c92b225 🚀

--- bin/build_me.sh
+++ bin/build_me.sh
@@ -4,16 +4,77 @@
 #                 if you are editing this file to make a local build work then the corresponding changes must be made in
 #                 the github workflow.
 
+# Get the name of the current git branch
+MYBRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+clean_branch()
+{
+#pull down the venv
+deactivate || source deactivate
+
+# Clean the things not tracked by git
+rm setup.md
+rm -r _site/ venv/ collections/ _includes/rsg/*-lesson/ slides/ _includes/ submodules/
+#find -f ./data \! -name "*.md" -depth 1 -delete # This is for the workshop only the data should be preserved in the lessons
+rm assets/favicons/rsg/apple* assets/favicons/rsg/favicon* assets/favicons/rsg/mstile*
+if [ 0 -lt $(ls _episodes_rmd/*.Rmd 2>/dev/null | wc -w) ]; then
+  rm _episodes/*.md _episodes/_page_built_on.html
+  rm -r _episodes_rmd/fig/
+  # These files are created in r-novice day 3
+  rm combo_plot_abun_weight.png name_of_file.png
+fi
+
+# Checkout main and cleanup branch
+git checkout $MYBRANCH
+git branch -d localbuild || echo 'branch local build does not exist to delete'
+git add -u
+git commit -m "cleanup"
+exit
+}
+
+#Trap the teardown to avoid poor state on build failure
+trap clean_branch 1 2 3 6
+
+case "$OSTYPE" in
+  solaris*) echo "OS SOLARIS not supported"; exit ;;
+  darwin*)  MYOS="OSX" ;;
+  linux*)   MYOS="LINUX" ;;
+  bsd*)     echo "OS BSD not supported"; exit ;;
+  msys*)    echo "OS WINDOWS not supported"; exit ;;
+  cygwin*)  echo "OS WINDOWS not supported"; exit ;;
+  *)        exit ;;
+esac
+
+if [ "$MYOS" = "LINUX" ]; then
+  read -p "Please provide package manager: " MYPKGMGR
+  sudo $MYPKGMGR install -y libcurl4-openssl-dev python3 python3-pip ruby ruby-dev libxml2
+  curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.bashrc
+fi;
+
+if [ "$MYOS" = "OSX" ]; then
+  read -p "Please confirm use of homebrew y/n: " HBOK
+  if [ "$HBOK" != "y" ]; then
+    exit
+  fi
+  brew install curl
+  brew install xml2
+  brew install ruby
+  brew install --cask jewelrybox
+fi
+
 # Make a branch to build on to avoid messing up main
 git branch -d localbuild || echo 'branch local build does not exist to delete'
 git checkout -b localbuild
 
-# Replicate GH actions
+# Replicate GH actions =================================================================================================
 python -m venv ./venv || echo 'venv already exists'
 
 #TODO: Make this windows safe
 source venv/bin/activate
 
+rvm install 2.7.1
+rvm use 2.7.1
 #TODO: Check why we need sudo here on ubuntu
 gem install github-pages bundler kramdown kramdown-parser-gfm
 
@@ -41,23 +102,8 @@ python bin/get_setup.py
 # Build the site.
 bundle install
 bundle exec jekyll serve --baseurl=""
-# All GH actions replicated
+# All GH actions replicated=============================================================================================
 
 #Note: the site is up here and will remain up until an interrupt (ctrl-c) is sent then the resto of this script triggers
 #      and cleans out the build.
-
-# Clean the things not tracked by git (Local Only: not replicated on GH actions)
-rm setup.md
-rm -r _site/ venv/ collections/ fig/ _includes/rsg/*-lesson/ slides/ _includes/ submodules/
-find -f ./data \! -name "*.md" -depth 1 -delete
-rm assets/favicons/rsg/apple* assets/favicons/rsg/favicon* assets/favicons/rsg/mstile*
-if ls _episodes_rmd/*.Rmd >/dev/null 2>&1; then
-  rm _episodes/*.md _episodes/_page_built_on.html
-  rm -r _episodes_rmd/fig/
-  # These files are created in r-novice day 3
-  rm combo_plot_abun_weight.png name_of_file.png
-fi
-
-# Checkout main and cleanup branch
-git checkout main
-git branch -d localbuild || echo 'branch local build does not exist to delete'
+clean_branch
